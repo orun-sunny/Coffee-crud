ff0dbd5c244db2de82ab10c20d503238fd8b0d9b
orun-sunny <sunny.mukut@gmail.com>
2024-09-18 23:10:26 +0600

    added crud

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,37 @@
-
-import { useLoaderData } from 'react-router-dom'
-import './App.css'
-import CoffeeCard from './components/CoffeeCard';
-import { useState } from 'react';
+import { Link, useLoaderData } from "react-router-dom";
+import "./App.css";
+import CoffeeCard from "./components/CoffeeCard";
+import { useState } from "react";
 
 function App() {
-
   const loadedCoffees = useLoaderData();
   const [coffees, setCoffees] = useState(loadedCoffees);
 
   return (
-    <div className='m-20'>
-      <h1 className='text-6xl text-center my-20 text-purple-600'>Hot Hot Cold Coffee: {coffees.length}</h1>
-      <div className='grid md:grid-cols-2 gap-4'>
-        {
-          coffees.map(coffee => <CoffeeCard
+    <div className="m-20">
+      <div className="flex space-x-4 items-center">
+        <h1 className="text-6xl text-center my-20 text-purple-600">
+          Coffee Store: {coffees.length}
+        </h1>
+        <Link to={`/addCoffee`}>
+          <button className="btn  px-6">Add Coffee</button>
+        </Link>
+        <Link to={`/users`}>
+          <button className="btn  px-8">Users</button>
+        </Link>
+      </div>
+      <div className="grid md:grid-cols-2 gap-4">
+        {coffees.map((coffee) => (
+          <CoffeeCard
             key={coffee._id}
             coffee={coffee}
             coffees={coffees}
             setCoffees={setCoffees}
-          ></CoffeeCard>)
-        }
+          ></CoffeeCard>
+        ))}
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
